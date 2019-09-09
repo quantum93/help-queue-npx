825898bfdf2b07b6024f365cabf42d5469d2b8d3
Student <example@example.com>
2019-09-09 13:17:44 -0700

    It is for Conditional Rendering

--- src/components/NewTicketControl.js
+++ src/components/NewTicketControl.js
@@ -1,20 +1,35 @@
 import React from 'react';
+import ConfirmationQuestions from './ConfirmationQuestions';
+import NewTicketForm from './NewTicketForm';
+
 
 class NewTicketControl extends React.Component {
 
   constructor(props) {
-  super(props);
-  this.state = {
-    formVisibleOnPage: false
-  };
-}
+    super(props);
+    this.state = {
+      formVisibleOnPage: false
+    };
+    // this.handleClick = this.handleClick.bind(this);
+  }
+
+  // handleClick(){
+  //   this.setState({formVisibleOnPage: true});
+  //   console.log('formVisibleOnPage is currently set to:' + this.state.formVisibleOnPage);
+  // }
 
   render(){
-    return (
-      <div>
-        <p>This is the NewTicketControl component!</p>
-      </div>
-    );
+      let currentlyVisibleContent = null;
+      if (this.state.formVisibleOnPage){
+        currentlyVisibleContent = <NewTicketForm />;
+      } else {
+        currentlyVisibleContent = <ConfirmationQuestions />;
+      }
+      return (
+        <div>
+          {currentlyVisibleContent}
+        </div>
+      );
   }
 }
 
