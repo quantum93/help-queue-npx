71f28a8ea7a60a834b7ee828df070a4c8ae8afe1
Student <example@example.com>
2019-09-09 11:54:53 -0700

    It is ready to dynamic state with npx scaffolding.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import TicketList from './components/TicketList';
+import NewTicketForm from './components/NewTicketForm';
+import Error404 from './components/Error404';
+import { Switch, Route } from 'react-router-dom';
 
-function App() {
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <Switch>
+        <Route exact path='/' component={TicketList} />
+        <Route path='/newticket' component={NewTicketForm} />
+        <Route component={Error404} />
+      </Switch>
     </div>
   );
 }
