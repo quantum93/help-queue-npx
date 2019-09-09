e2b4285076e1b77c3dfd042be6e13de5e19ff4be
Student <example@example.com>
2019-09-09 13:39:56 -0700

    It works with Lifting State

--- src/components/NewTicketControl.js
+++ src/components/NewTicketControl.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ConfirmationQuestions from './ConfirmationQuestions';
 import NewTicketForm from './NewTicketForm';
+import PropTypes from "prop-types";
+
 
 
 class NewTicketControl extends React.Component {
@@ -20,8 +22,8 @@ class NewTicketControl extends React.Component {
   render(){
       let currentlyVisibleContent = null;
       if (this.state.formVisibleOnPage){
-        currentlyVisibleContent = <NewTicketForm />;
-      } else {
+         currentlyVisibleContent = <NewTicketForm onNewTicketCreation={this.props.onNewTicketCreation}/>;
+       } else {
         currentlyVisibleContent = <ConfirmationQuestions onTroubleshootingConfirmation={this.handleTroubleshootingConfirmation}/>;
       }
       return (
@@ -31,5 +33,9 @@ class NewTicketControl extends React.Component {
       );
   }
 }
+
+NewTicketControl.propTypes = {
+  onNewTicketCreation: PropTypes.func
+};
 
 export default NewTicketControl;
