63052a38dff56aaf905110e2b4843cb3dae248af
Student <example@example.com>
2019-09-11 08:11:09 -0700

    It works with Moment.js

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -6,7 +6,6 @@ import PropTypes from 'prop-types';
 
 
 function TicketList(props){
-  console.log(props.ticketList);
   return (
     <div>
       <hr/>
@@ -14,6 +13,7 @@ function TicketList(props){
         <Ticket names={ticket.names}
           location={ticket.location}
           issue={ticket.issue}
+          timeOpen={ticket.timeOpen}
           key={ticket.id}/>
       )}
     </div>
